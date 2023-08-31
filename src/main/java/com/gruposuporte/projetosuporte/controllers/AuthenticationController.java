812db36bb68e6cf389ba66f2b072fed12d8d055d
Jardson Costa <jardsoncostasilva@gmail.com>
2023-08-31 14:36:15 -0300

    ajustando controllers

--- src/main/java/com/gruposuporte/projetosuporte/controllers/AuthenticationController.java
+++ src/main/java/com/gruposuporte/projetosuporte/controllers/AuthenticationController.java
@@ -1,21 +1,28 @@
 package com.gruposuporte.projetosuporte.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
-public class RegistroController {
+public class AuthenticationController {
 
     @GetMapping("/register")
-    public String index(){
-        return "register/index";
+    public String register() {
+        return "register";
     }
 
     @GetMapping("/register/agent") // metodo para acessar tela de cadastro do agente
-    public String registerAgent(){
-        return "register/index";
+    public String registerAgent() {
+        return "register";
+    }
+
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
     }
 
 }
