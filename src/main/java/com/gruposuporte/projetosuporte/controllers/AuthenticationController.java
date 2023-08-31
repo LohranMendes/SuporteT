29b3196aef36eaf5c5a5477d7d79b936084c42e5
lohranmendes <lohranfrancisco@outlook.com>
2023-08-31 16:45:48 -0300

    Telas em HTML

--- src/main/java/com/gruposuporte/projetosuporte/controllers/AuthenticationController.java
+++ src/main/java/com/gruposuporte/projetosuporte/controllers/AuthenticationController.java
@@ -3,10 +3,8 @@ package com.gruposuporte.projetosuporte.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class AuthenticationController {
 
     @GetMapping("/register")
@@ -14,9 +12,9 @@ public class AuthenticationController {
         return "register";
     }
 
-    @GetMapping("/register/agent") // metodo para acessar tela de cadastro do agente
+    @GetMapping("/agent") // metodo para acessar tela de cadastro do agente
     public String registerAgent() {
-        return "register";
+        return "agent";
     }
 
 
